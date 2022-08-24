acc8aac0fc6b9db71f0e4aff49150ec8ab6da01a
Kevin Hugo <hugo@mtn-fm.id>
2022-08-24 19:19:04 +0700

    Initial Commit

--- func.go
+++ func.go
@@ -3,11 +3,20 @@ import "fmt"
 
 func main() {
 	names := []string{"Mr","Sir","Bro","Friend","Kevin","Hugo"}
-	printNames(names)
+	var pointers []*string
+	for key, _ := range names {
+		pointers = append(pointers, &names[key])
+	}
+	printNames := func (points []*string) {
+		for index, eachPoint := range points {
+			fmt.Println(index+1,*eachPoint)
+		}
+	}
+	printNames(pointers)
 }
 
-func printNames(names []string) {
-	for index, eachName := range names {
-		fmt.Println(index+1,eachName)
-	}
-}
+// func printNames(names []string) {
+// 	for index, eachName := range names {
+// 		fmt.Println(index+1,eachName)
+// 	}
+// }
